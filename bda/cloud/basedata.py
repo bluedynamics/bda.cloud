eb8fbc3396193611832426aae46aaa7ab95d27f3
thet <thet@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-01-20 15:14:19 +0000

    foo

--- bda/cloud/basedata.py
+++ bda/cloud/basedata.py
@@ -13,37 +13,42 @@ from interfaces import ICloudData
 
 class BaseData(object):
     """abstract base class for lazy programmers."""
-    
-    implements(ICloudData)    
-    
+
+    implements(ICloudData)
+
     def __init__(self, context):
         self.context = context
         self.data = None
-        
+
     def querydata(self):
         """query data from catalog"""
         raise NotImplementedError, "querydata must be implemented in subclass."
-    
+
     def keys(self):
         """returns all keys in data"""
         self.querydata()
         return self.data.keys()
-        
+
     def amountOf(self, key):
         """the amount of a key in data"""
         self.querydata()
         return self.data[key]
-        
+
     def minAmount(self):
         """the lowest amount"""
         self.querydata()
         ams = [self.data[key] for key in self.data.keys()]
         ams.sort()
-        return ams[0]
-        
+        if ams:
+            return ams[0]
+        else:
+            return 0
+
     def maxAmount(self):
         """the largest amount"""
         ams = [self.data[key] for key in self.data.keys()]
         ams.sort(reverse=True)
-        return ams[0]
-    
+        if ams:
+            return ams[0]
+        else:
+            return 0
