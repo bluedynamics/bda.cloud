2fb1ee15c0d4eac226ab78b3d32374a6443ca3f4
jensens <jensens@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-04-08 14:26:07 +0000

    prepare a release - further steps

--- setup.py
+++ setup.py
@@ -4,27 +4,28 @@
 from setuptools import setup, find_packages
 
 version = '2.0'
+shortdesc = 'Genric Tag-Cloud for Plone'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
 
 setup(name='bda.cloud',
       version=version,
-      description="",
-      long_description="""\
-""",
+      description=shortdesc,
+      long_description=longdesc,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Framework :: Zope2",
-        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
       author='BlueDynamics Alliance',
       author_email='dev@bluedynamics.com',
-      url='http://svn.bluedynamics.net/svn/internal',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['bda'],
+      url='http://svn.plone.org/svn/collective/bda.cloud',
+      license='GPL',    
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['bda',],            
       include_package_data=True,
       zip_safe=False,
       install_requires=[
